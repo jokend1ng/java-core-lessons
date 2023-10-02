c05be84d2cf3a8641c464c8ddfced2036f1ba4d7
dasha <dasha@gmail.com>
2023-10-02 15:43:45 +0300

    Занятие №1

---
+++ src/lesson01/Lesson01.java
@@ -0,0 +1,9 @@
+package lesson01;
+
+public class Lesson01 {
+    // psvm + Enter
+    public static void main(String[] args) {
+        // sout + Enter
+        System.out.println("Lesson 01");
+    }
+}
