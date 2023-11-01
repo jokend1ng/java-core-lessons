6c96331ada3f5a4c81d9a3d7e798f499e0b73f43
dasha <dasha@gmail.com>
2023-11-01 22:30:51 +0300

    Занятие #15

---
+++ src/com/company/project/lesson1516/Node.java
@@ -0,0 +1,30 @@
+package com.company.project.lesson1516;
+
+class MyLinkedListNode<T> {
+    // значение, которые добавляется в связный список
+    private T value;
+    // ссылка на следующую ноду (узел)
+    private MyLinkedListNode<T> nextNode;
+    // ссылка на предыдущую ноду (узел), если список двунаправленный
+    private MyLinkedListNode<T> previousNode;
+
+    public MyLinkedListNode(T value) {
+        this.value = value;
+    }
+
+    public MyLinkedListNode<T> getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(MyLinkedListNode<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    public MyLinkedListNode<T> getPreviousNode() {
+        return previousNode;
+    }
+
+    public void setPreviousNode(MyLinkedListNode<T> previousNode) {
+        this.previousNode = previousNode;
+    }
+}
