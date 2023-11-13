e1d6a2b5aae85fb819c7bce8365fc2a8faa5886f
dasha <dasha@gmail.com>
2023-11-13 22:40:07 +0300

    Занятие #19

---
+++ src/com/company/project/lesson19/task/task03/Task03.java
@@ -0,0 +1,30 @@
+package com.company.project.lesson19.task.task03;
+
+import java.util.*;
+
+
+public class Task03 {
+    public static void main(String[] args) {
+        // Написать реализацию методов task01 - task04
+        // Для некоторых решений могут пригодиться методы flatMap / flatMapToInt / map / mapToInt класса Stream
+    }
+
+    public static Double task01(ArrayList<Article> articles){
+        // Вернуть средний возраст авторов статей
+        return null;
+    }
+
+    public static List<String> task02(ArrayList<Article> articles, Article.Category category){
+        // Вернуть список email авторов статей категории == category
+        return null;
+    }
+    public static List<Article> task03(ArrayList<Article> articles, Article.Category category, int min, int max){
+        // Вернуть список статей категории == category,
+        // возраст авторов которых попадает в диапазон от min до max
+        return null;
+    }
+    public static List<Article> task04(ArrayList<Article> articles, Article.Category category){
+        // Вернуть список статей категории == category, опубликованных сегодня
+        return null;
+    }
+}
